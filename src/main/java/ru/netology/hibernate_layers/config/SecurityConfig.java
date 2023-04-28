a0a4346c3f22e11bb96c4478aa6c04e5a10deab3
Dima Vorobev <dimjkee@icloud.com>
2023-04-28 20:26:55 +0300

    add security_method

--- src/main/java/ru/netology/hibernate_layers/config/SecurityConfig.java
+++ src/main/java/ru/netology/hibernate_layers/config/SecurityConfig.java
@@ -2,19 +2,17 @@ package ru.netology.hibernate_layers.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -22,18 +20,33 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http.formLogin().and().authorizeHttpRequests().requestMatchers("/persons").permitAll().and().authorizeHttpRequests().requestMatchers("/persons/by-city", "/persons/by-age").hasAnyAuthority("USER", "ADMIN").and().authorizeHttpRequests().requestMatchers("/persons/").hasAuthority("ADMIN").and().authorizeHttpRequests().anyRequest().authenticated().and().build();
-
-
-    }
-
-    @Bean
     public UserDetailsManager userDetailsService() {
-        UserDetails admin = User.withUsername("Dima").password(passwordEncoder().encode("112233")).authorities("ADMIN").build();
-        UserDetails guest = User.withUsername("User").password(passwordEncoder().encode("0000")).authorities("USER").build();
+        UserDetails userDima = User
+                .withUsername("Dima")
+                .password(passwordEncoder().encode("112233"))
+                .roles("READ", "WRITE", "DELETE")
+                .build();
+        UserDetails userAnton = User
+                .withUsername("Anton")
+                .password(passwordEncoder().encode("0000"))
+                .authorities("READ", "WRITE")
+                .build();
+        UserDetails userIvan = User
+                .withUsername("Ivan")
+                .password(passwordEncoder().encode("0000"))
+                .roles("WRITE")
+                .build();
+        UserDetails userPavel = User
+                .withUsername("Pavel")
+                .password(passwordEncoder().encode("0000"))
+                .roles("WRITE")
+                .build();
+        UserDetails userSlava = User
+                .withUsername("Slava")
+                .password(passwordEncoder().encode("0000"))
+                .roles("DELETE")
+                .build();
 
-        return new InMemoryUserDetailsManager(admin, guest);
+        return new InMemoryUserDetailsManager(userDima, userAnton, userIvan, userPavel, userSlava);
     }
-
 }
--- src/main/java/ru/netology/hibernate_layers/controller/GetController.java
+++ src/main/java/ru/netology/hibernate_layers/controller/GetController.java
@@ -1,7 +1,11 @@
 package ru.netology.hibernate_layers.controller;
 
+import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.*;
 import ru.netology.hibernate_layers.Entity.Person;
 import ru.netology.hibernate_layers.service.GetService;
@@ -10,34 +14,40 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/persons")
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class GetController {
     public final GetService getService;
-    @GetMapping("/by-city")
+    @Secured({"ROLE_READ"})
+    @GetMapping("/persons/by-city")
     public List<Person> getPersonsByCity(@RequestParam(value = "city") String city) {
         return getService.getPersonsByCity(city);
     }
-    @GetMapping("/by-age")
+    @PreAuthorize("hasAuthority('READ')")
+    @GetMapping("/persons/by-age")
     public List<Person> getPersonsByAge(@RequestParam(value = "age") int age) {
         return getService.getPersonsByAge(age);
     }
-    @GetMapping("/by-name-surname")
+    @PreAuthorize("hasAnyAuthority('READ','WRITE')")
+    @GetMapping("/persons/by-name-surname")
     public Optional<Person> getPersonsByNameAndSurname(@RequestParam(value = "name") String name, @RequestParam(value = "surname") String surname) {
         return getService.getPersonsByNameAndSurname(name, surname);
     }
 
-    @PostMapping("/create")
+    @RolesAllowed({"ROLE_WRITE"})
+    @PostMapping("/persons/create")
     public Person create (@RequestBody Person person){
         return getService.create(person);
     }
 
-    @PutMapping("/update")
-    public Person update (@RequestBody Person person){
-        return getService.update(person);
-    }
-    @DeleteMapping("/delete")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
+    @DeleteMapping("/persons/delete")
     public String delete (@RequestBody Person person){
        return getService.delete(person);
+    }
+    @GetMapping("/persons/lk")
+    @PreAuthorize("#username == authentication.principal.username")
+    public String greetingUser(String username) {
+        return "Hello " + username + " from secure app!" ;
     }
 }
